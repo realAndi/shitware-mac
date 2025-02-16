687b3f2957acdc356bf6deca62bcd4a38975f512
realAndi <tafilajandi@gmail.com>
2025-02-15 20:21:38 -0500

    Initial Commit

---
+++ Shitware/ShitwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShitwareApp.swift
+//  Shitware
+//
+//  Created by andi on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ShitwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShitwareTests/ShitwareTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShitwareTests.swift
+//  ShitwareTests
+//
+//  Created by andi on 2/15/25.
+//
+
+import Testing
+@testable import Shitware
+
+struct ShitwareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
